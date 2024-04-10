19e841608c07bbe4e25b6f8430805f826bc95397
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-09 22:23:49 -0300

    Hello World do caralho

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, Mundo!");
+    }
+}
